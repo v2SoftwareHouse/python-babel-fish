3110fed360fbfb7fb9bdb217365c10ef15423271
Caio Sanchez <caioschristino@gmail.com>
2024-03-21 19:38:15 -0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name='Babel-Fish',
+    name='babelfish',
     packages=find_packages(include=['BabelFish']),
     version='0.0.1',
     description='A framework to drive a clean code',
